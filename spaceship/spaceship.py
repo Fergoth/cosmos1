13fe57ce5e156c5bb7b10daa6cb1ed718e632c8a
fergoth <fergothew@gmail.com>
2025-11-05 15:50:00 +0300

    add acseleration physics

--- spaceship/spaceship.py
+++ spaceship/spaceship.py
@@ -2,6 +2,7 @@ import asyncio
 import curses
 from itertools import cycle
 from utilities import draw_frame, read_controls, get_frame_size, duplicate_frames
+from physics import update_speed
 
 SPACE_SHIP_TICK_RATE = 0.1
 
@@ -21,6 +22,7 @@ async def animate_spaceship(
     frames: list,
     tick_timeout: float,
 ):
+    row_speed = col_speed = 0
     col = start_col
     row = start_row
     frame_rate = int(SPACE_SHIP_TICK_RATE / tick_timeout)
@@ -29,8 +31,9 @@ async def animate_spaceship(
         await asyncio.sleep(0)
         row_dir, col_dir, _ = read_controls(canvas)
         draw_frame(canvas, row, col, frame, negative=True)
-        row += row_dir
-        col += col_dir
+        row_speed, col_speed = update_speed(row_speed, col_speed, row_dir, col_dir)
+        row += row_speed
+        col += col_speed
         max_row, max_col = canvas.getmaxyx()
         frame_rows, frame_cols = get_frame_size(frame)
         row = max(0, min(row, max_row - frame_rows))
