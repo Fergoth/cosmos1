009f6395fd2d9583556c96232bf1c65dc607ad56
fergoth <fergothew@gmail.com>
2025-11-05 15:09:19 +0300

    add garbage

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import time
 import curses
-from stars.stars import add_stars
+from stars import add_stars
 from spaceship.spaceship import load_ship_frames, animate_spaceship
 from spaceship.fire import fire
-
+from garbage import load_garbage_frames, fly_garbage
+from random import choice
 
 TICK_TIMEOUT = 0.1
 
@@ -11,11 +12,15 @@ TICK_TIMEOUT = 0.1
 def draw(canvas: curses.window):
     canvas.nodelay(True)
     ship_frames = load_ship_frames()
+    garbage_frames = load_garbage_frames()
     curses.curs_set(0)
     coroutines = []
     coroutines = add_stars(canvas, coroutines, TICK_TIMEOUT)
     coroutines.append(fire(canvas, 10, 10))
     coroutines.append(animate_spaceship(canvas, 10, 10, ship_frames, TICK_TIMEOUT))
+    coroutines.append(
+        fly_garbage(canvas, column=10, garbage_frame=choice(garbage_frames))
+    )
     while True:
         for coroutine in coroutines.copy():
             try:
